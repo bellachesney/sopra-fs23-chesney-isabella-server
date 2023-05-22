96beeacaf4c673dd3a569043eb1ca9b460c0d219
bellachesney <isabella.chesney.s@gmail.co>
2023-05-22 14:36:59 +0200

    urls eändern

--- src/main/java/ch/uzh/ifi/hase/soprafs23/websocket/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/websocket/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private static final String WEBSOCKET_SUFFIX = "/websocket";
     private static final String DESTINATION_PREFIX = "/game";
     private static final String ORIGIN_LOCALHOST = "http://localhost:3000";
-    private static final String ORIGIN_PROD = "https://sopra-fs23-group-37-client.oa.r.appspot.com/";
+    private static final String ORIGIN_PROD = "https://sopra-fs23-ichesn-client.oa.r.appspot.com/";
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
